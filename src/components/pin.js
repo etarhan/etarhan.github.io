cc1e83e1b452995eeb6e27411ff7e81b20335ad3
etarhan <eraytarhan3@gmail.com>
2019-03-11 03:13:31 +0300

    Add background, font changes and responsiveness

--- src/components/pin.js
+++ src/components/pin.js
@@ -1,19 +1,19 @@
-import React, { useRef } from 'react';
-import { animated, useSpring, useChain, config } from 'react-spring';
+import React, { useRef, useState } from 'react';
+import { animated, useSpring, useChain, config, interpolate } from 'react-spring';
 
-const Pin = ({ children }) => {
+const Pin = ({ url, children }) => {
   const firstRef = useRef();
   const firstProps = useSpring({
     ref: firstRef,
-    to: { width: '100%', opacity: 1 },
-    from: { width: '0%', opacity: 0 },
-    config: config.molasses
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: config.gentle
   });
 
   const secondRef = useRef();
   const secondProps = useSpring({
     ref: secondRef,
-    to: { borderRadius: '50%'},
+    to: { borderRadius: '50%' },
     from: { borderRadius: '0%' },
     // delay: 100,
     config: config.gentle
@@ -21,10 +21,28 @@ const Pin = ({ children }) => {
 
   useChain([firstRef, secondRef], [0.5, 1]);
 
+  // const [bounce, setBounce] = useState(false)
+  const [grow, setGrow] = useState(false)
+
+  const growProps = useSpring({ 
+    transform: `scale(${grow ? 1.1 : 1}`, 
+    config: config.gentle
+  });
+
   return (
-    <animated.span style={{ ...firstProps, ...secondProps }} className="pin">
+
+    <animated.a 
+      href={url}
+      style={{
+        ...firstProps,
+        ...secondProps,
+        ...growProps
+      }}
+      className="pin" 
+      onMouseOver={() => setGrow(true)}
+      onMouseLeave={() => setGrow(false)}>
       {children}
-    </animated.span>
+    </animated.a>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,62 @@
-import React, { useRef } from 'react';
-import { FaLinkedin, FaGithub, FaStackOverflow } from 'react-icons/fa';
+import React, { useRef, useState } from 'react';
+import { FaLinkedin, FaGithub, FaStackOverflow, FaAngleDown } from 'react-icons/fa';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Pin from "../components/pin";
 import { animated, useSpring, useChain, config } from 'react-spring';
 
 const IndexPage = () => {
-  
+  const headerProps = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: config.molasses
+  });
+
+  const [shown, setShown] = useState(false);
+
+  const dotProps = useSpring({
+    to: { opacity: shown ? 0  : 1 },
+    from: { opacity: shown ? 1  : 0 },
+    onRest: () => setShown(!shown),
+    config: config.slow
+  });
+
+  // const secondRef = useRef();
+  // const secondProps = useSpring({
+  //   ref: secondRef,
+  //   to: { borderRadius: '50%' },
+  //   from: { borderRadius: '0%' },
+  //   // delay: 100,
+  //   config: config.gentle
+  // });
 
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div className="header-parallax">
-        <div className="center-content">
-          <div className="text-container">
-            <animated.h1>Eray Tarhan</animated.h1>
-            <animated.h2>Software Developer</animated.h2>
+      <div className="container-fluid h-100 header-parallax">
+        <div className="row h-75 justify-content-center align-items-end">
+          <div className="col-sm-12 text-center text-container">
+            <animated.h1 style={headerProps}>Eray<animated.span style={dotProps}>·</animated.span>Tarhan</animated.h1>
+            <animated.h2 style={headerProps}>Full-stack Web Developer</animated.h2>
           </div>
-          <div className="pin-container">
-            <Pin>
+          <div className="col-sm-12 text-center">
+            <Pin url="https://www.linkedin.com/in/etarhan">
               <FaLinkedin size="5em" />
             </Pin>
-            <Pin>
+            <Pin url="https://github.com/etarhan">
               <FaGithub size="5em" />
             </Pin>
-            <Pin>
+            <Pin url="https://stackoverflow.com/users/10353987/etarhan">
               <FaStackOverflow size="5em" />
             </Pin>
           </div>
         </div>
+        {/*<div className="row h-25 align-items-end">
+          <div className="col-sm-12 text-center">
+            <h5>See more</h5>
+            <FaAngleDown className="chevron-down" size="3em" />
+          </div>
+  </div>*/}
       </div>
     </Layout>
   );
