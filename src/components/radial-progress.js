56a1a83a472d9ae0fb897dfc5d8d3a45c84f533a
etarhan <eraytarhan3@gmail.com>
2019-03-12 18:51:25 +0300

    Add own custom radial progress bar

---
+++ src/components/radial-progress.js
@@ -0,0 +1,84 @@
+import React, { useRef } from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+const circleCalc = (inputSize, unit) => css`
+  content: '';
+  position: absolute;
+  border-radius: 50%;
+  left: calc(50% - ${inputSize / 2 + unit});
+  top: calc(50% - ${inputSize / 2 + unit});
+  width: ${inputSize + unit};
+  height: ${inputSize + unit};
+`;
+
+export default ({
+  percent,
+  size,
+  ratioInner = 1.5,
+  color = '#81ce97',
+  backColor = '#e5e5e5',
+  innerColor = '#f0f0f5',
+  children,
+  className,
+}) => {
+  const sizeNumber = size.match(/[0-9]+/)[0];
+  const sizeUnit = size.match(/[A-Za-z]+/)[0];
+
+  return (
+    <>
+      <div
+        css={css`
+          width: ${size};
+          height: ${size};
+          border-radius: 50%;
+          background-color: ${percent > 50 ? color : backColor};
+          position: relative;
+          box-shadow: 0px 0px 15px -4px ${color};
+        `}
+        className={className}
+      >
+        <div
+          css={css`
+              ${circleCalc(sizeNumber, sizeUnit)}
+              clip: ${
+                percent > 50
+                  ? `rect(0, ${sizeNumber / 2 + sizeUnit}, ${size}, 0)`
+                  : `rect(0, ${size}, ${size}, ${sizeNumber / 2 + sizeUnit})`
+              };
+          `}
+        >
+          <div
+            css={css`
+              ${circleCalc(sizeNumber, sizeUnit)}
+              clip: ${
+                percent > 50
+                  ? `rect(0, ${size}, ${size}, ${sizeNumber / 2 + sizeUnit})`
+                  : `rect(0, ${sizeNumber / 2 + sizeUnit}, ${size}, 0)`
+              };
+              background-color: ${percent > 50 ? backColor : color};
+              transform: rotate(${(360 * parseInt(percent)) / 100}deg);
+            `}
+          />
+        </div>
+        <div
+          css={css`
+            ${circleCalc(sizeNumber / ratioInner, sizeUnit)}
+            background: ${innerColor};
+            text-align: center;
+            display: table;
+          `}
+        >
+          <div
+            css={css`
+              display: table-cell;
+              vertical-align: middle;
+            `}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,78 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { 
-  FaLinkedin, 
-  FaGithub, 
-  FaStackOverflow, 
-  FaAngleDown, 
-  FaNodeJs, 
-  FaJs, 
-  FaHtml5, 
-  FaReact, 
-  FaCss3Alt, 
-  FaCode 
-} from 'react-icons/fa';
-import { Circle } from 'rc-progress';
+import React, { useState, useRef, useEffect } from "react"
+import {
+  FaLinkedin,
+  FaGithub,
+  FaStackOverflow,
+  FaAngleDown,
+  FaNodeJs,
+  FaJs,
+  FaHtml5,
+  FaReact,
+  FaCss3Alt,
+  FaCode,
+} from "react-icons/fa"
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core"
+import styled from "@emotion/styled"
+import { Circle } from "rc-progress"
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Pin from "../components/pin";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Pin from "../components/pin"
 import Header from "../components/header"
+import RadialProgress from "../components/radial-progress"
 
-import { animated, useSpring, config } from 'react-spring';
+import { animated, useSpring, config } from "react-spring"
+
+const SkillDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 1em;
+`
 
 const IndexPage = () => {
   const headerProps = useSpring({
     to: { opacity: 1 },
     from: { opacity: 0 },
-    config: config.molasses
-  });
+    config: config.molasses,
+  })
 
-  const [shown, setShown] = useState(false);
+  const [shown, setShown] = useState(false)
 
   const dotProps = useSpring({
     to: { opacity: shown ? 0 : 1 },
     from: { opacity: shown ? 1 : 0 },
     onRest: () => setShown(!shown),
-    config: config.slow
-  });
+    config: config.slow,
+  })
 
-  const scrollToDiv = useRef(null);
+  const scrollToDiv = useRef(null)
 
   const [sectionTop, setSectionTop] = useState(0)
   useEffect(() => {
-    const section = document.getElementsByClassName('skill-section')[0];
-    const nav = document.getElementsByClassName('main-header')[0];
+    const section = document.getElementsByClassName("skill-section")[0]
+    const nav = document.getElementsByClassName("main-header")[0]
 
-    if (section.offsetTop > 0)
-      setSectionTop(section.offsetTop);
+    if (section.offsetTop > 0) setSectionTop(section.offsetTop)
 
     const stickyEvent = () => {
       if (window.scrollY >= sectionTop) {
-        nav.classList.add('fixed-nav');
+        nav.classList.add("fixed-nav")
       } else {
-        if (nav.classList.contains('fixed-nav')) {
-          nav.classList.add('hidden-nav');
-          nav.classList.remove('fixed-nav');
+        if (nav.classList.contains("fixed-nav")) {
+          nav.classList.add("hidden-nav")
+          nav.classList.remove("fixed-nav")
         }
       }
-    };
-    window.addEventListener('scroll', stickyEvent);
+    }
+    window.addEventListener("scroll", stickyEvent)
 
     return () => {
-      window.removeEventListener("scroll", stickyEvent);
-    };
-  });
-
+      window.removeEventListener("scroll", stickyEvent)
+    }
+  })
 
   return (
     <Layout>
@@ -70,8 +80,12 @@ const IndexPage = () => {
       <div className="container-fluid header-parallax">
         <section className="row h-75 justify-content-center align-items-end">
           <div className="col-sm-12 text-center text-container">
-            <animated.h1 style={headerProps}>Eray<animated.span style={dotProps}>·</animated.span>Tarhan</animated.h1>
-            <animated.h2 style={headerProps}>Full-stack Web Developer</animated.h2>
+            <animated.h1 style={headerProps}>
+              Eray<animated.span style={dotProps}>·</animated.span>Tarhan
+            </animated.h1>
+            <animated.h2 style={headerProps}>
+              Full-stack Web Developer
+            </animated.h2>
           </div>
           <div className="col-sm-12 text-center">
             <Pin url="https://www.linkedin.com/in/etarhan">
@@ -92,53 +106,109 @@ const IndexPage = () => {
           </div>
         </div>
         <section className="row h-100">
-          <div ref={scrollToDiv} id="skills" className="col-sm-12 skill-section">
+          <div
+            ref={scrollToDiv}
+            id="skills"
+            className="col-sm-12 skill-section"
+          >
             <Header />
             <div className="headtext text-center">
               <h1>Skills</h1>
-              <div className="understroke"></div>
+              <div className="understroke" />
             </div>
             <div className="skills-container">
               <div className="skills-description">
-                <div className="text-center">
-                </div>
-                <p>I'm an experienced developer well versed in a wide range of technologies. Here is a subselection of what I like using nowadays.</p>
+                <div className="text-center" />
+                <p>
+                  I'm an experienced developer well versed in a wide range of
+                  technologies. Here is a subselection of what I like using
+                  nowadays.
+                </p>
               </div>
-              <FaCode size="5em" className="back-graphic"/>
+              <FaCode size="5em" className="back-graphic" />
               <div className="skills">
-                <div className="skill">
-                  <Circle percent="90" strokeWidth="15" trailWidth="8" strokeColor="green" strokeLinecap="butt" />
-                  <span><FaNodeJs size="5em" /></span>
-                  <span>Node.js</span>
-                </div>
-                <div className="skill">
-                  <Circle percent="85" strokeWidth="15" trailWidth="8" strokeColor="#007bff" strokeLinecap="butt" />
-                  <span><FaReact size="5em" /></span>
-                  <span>React</span>
-                </div>
-                <div className="skill">
-                  <Circle percent="95" strokeWidth="15" trailWidth="8" strokeColor="orange" strokeLinecap="butt" />
-                  <span><FaHtml5 size="5em" /></span>
-                  <span>HTML5</span>
-                </div>
-                <div className="skill">
-                  <Circle percent="90" strokeWidth="15" trailWidth="8" strokeColor="#264de4" strokeLinecap="butt" />
-                  <span><FaCss3Alt size="5em" /></span>
-                  <span>CSS3</span>
-                </div>
-                <div className="skill">
-                  <Circle percent="90" strokeWidth="15" trailWidth="8" strokeColor="#f0db4f" strokeLinecap="butt" />
-                  <span><FaJs size="5em" /></span>
-                  <span>JavaScript</span>
-                </div>
+                <RadialProgress
+                  css={css`
+                    margin: 1em;
+                  `}
+                  percent={80}
+                  size="9em"
+                  color="#026e00"
+                >
+                  <SkillDescription>
+                    <span>
+                      <FaReact size="2em" />
+                    </span>
+                    <span>Node.js</span>
+                  </SkillDescription>
+                </RadialProgress>
+                <RadialProgress
+                  css={css`
+                    margin: 1em;
+                  `}
+                  percent={85}
+                  size="9em"
+                  color="#61dafb"
+                >
+                  <SkillDescription>
+                    <span>
+                      <FaReact size="2em" />
+                    </span>
+                    <span>React</span>
+                  </SkillDescription>
+                </RadialProgress>
+                <RadialProgress
+                  css={css`
+                    margin: 1em;
+                  `}
+                  percent={90}
+                  size="9em"
+                  color="#f16529"
+                >
+                  <SkillDescription>
+                    <span>
+                      <FaHtml5 size="2em" />
+                    </span>
+                    <span>HTML5</span>
+                  </SkillDescription>
+                </RadialProgress>
+                <RadialProgress
+                  css={css`
+                    margin: 1em;
+                  `}
+                  percent={87}
+                  size="9em"
+                  color="#264de4"
+                >
+                  <SkillDescription>
+                    <span>
+                      <FaCss3Alt size="2em" />
+                    </span>
+                    <span>CSS3</span>
+                  </SkillDescription>
+                </RadialProgress>
+                <RadialProgress
+                  css={css`
+                    margin: 1em;
+                  `}
+                  percent={93}
+                  size="9em"
+                  color="#f0db4f"
+                >
+                  <SkillDescription>
+                    <span>
+                      <FaJs size="2em" />
+                    </span>
+                    <span>JavaScript</span>
+                  </SkillDescription>
+                </RadialProgress>
               </div>
             </div>
           </div>
         </section>
       </div>
     </Layout>
-  );
+  )
 }
-
 
 export default IndexPage
