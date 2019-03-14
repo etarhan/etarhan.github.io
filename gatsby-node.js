a1385c9e4746ef704a797a2f556d9be881c6a650
etarhan <eraytarhan3@gmail.com>
2019-03-14 04:54:01 +0300

    Add IE11 support

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  stage,
+  getConfig,
+  actions: { replaceWebpackConfig }
+}) => {
+  switch (stage) {
+    case 'build-javascript':
+      // We want to include the babel polyfills before any application code,
+      // so we're inserting it as an additional entry point.  Gatsby does not allow
+      // this in "setWebpackConfig", so we have to use "replaceWebpackConfig"
+      const config = getConfig();
+
+      const app =
+        typeof config.entry.app === 'string'
+          ? [config.entry.app]
+          : config.entry.app;
+
+      config.entry.app = ['@babel/polyfill', ...app];
+      replaceWebpackConfig(config);
+  }
+};
